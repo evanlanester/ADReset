3689b0961c11a904cde48ef2669b38bb1371c600
ejlane <evan.james.lane@gmail.com>
2023-01-12 20:14:56 -0400

    Update Crypto.php

--- resources/classes/Crypto.php
+++ resources/classes/Crypto.php
@@ -7,9 +7,13 @@
                 $key = Config::get('security/encryptionKey');
                 $encrypt = serialize($encrypt);
                 $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
+                $ic = random_bytes()
+
                 $key = @pack('H*', $key);
                 $mac = hash_hmac('sha256', $encrypt, substr(bin2hex($key), -32));
                 $passcrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $encrypt.$mac, MCRYPT_MODE_CBC, $iv);
+                $passcrypt = openssl_encrypt(aes-256-ctr, $key, $encrypt.$mac, $iv);
+
                 $encoded = base64_encode($passcrypt).'|'.base64_encode($iv);
                 return $encoded;
             }
